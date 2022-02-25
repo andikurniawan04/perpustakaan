6d3a5ba7236a8dfd4b9c1359dd5db478c7ff41aa
Maulana Anwar <mauzen.rs@gmail.com>
2022-02-25 22:32:05 +0700

    Memperbaiki migrasi table

---
+++ database/migrations/2022_02_25_145436_create_meminjams_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateMeminjamsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('meminjams', function (Blueprint $table) {
+            $table->id('id_peminjaman');
+            $table->unsignedBigInteger('id_user');
+            $table->unsignedBigInteger('id_buku');
+            $table->date('tanggal_pinjam');
+            $table->date('tanggal_kembali');
+            $table->integer('jumlah');
+            $table->foreign('id_user')->references('id_user')->on('users');
+            $table->foreign('id_buku')->references('id_buku')->on('bukus');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('meminjams');
+    }
+}
