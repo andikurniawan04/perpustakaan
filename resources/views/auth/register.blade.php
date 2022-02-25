26d7702214f0522d07eb69e4c797795fc7fdf7e8
rasiyahshafa <rasiyahshafa@gmail.com>
2022-02-25 23:06:20 +0700

    Backend Regist

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -20,8 +20,8 @@
                                         <div class="text-center">
                                             <h1 class="h4 text-gray-900 mb-4">Create an Account!</h1>
                                         </div>
-                                        <form action="/register" method="POST" class="user">
-                                        {{csrf_field()}}
+                                        <form action="{{ route('register.store')}}" method="POST" class="user" enctype="multipart/form-data">
+                                        @csrf
                                         <div class="form-group row">
                                     <div class="col-sm-12 mb-3 mb-sm-0">
                                         <input type="text" name="name" class="form-control form-control-user" id="name"
@@ -38,9 +38,9 @@
                                             id="password" placeholder="Password">
                                     </div>
                                 </div>
-                                            <a href="index.html" class="btn btn-primary btn-user btn-block">
+                                            <button type="submit" class="btn btn-primary">
                                                 Register Account
-                                            </a>
+                                            </button>
                                             <hr>
                                             <a href="index.html" class="btn btn-google btn-user btn-block">
                                                 <i class="fab fa-google fa-fw"></i> Register with Google
