354ba457857e9346be7ccf6fdb39155d7713819a
wahyustiyawan <100087469+wahyustiyawan@users.noreply.github.com>
2022-02-28 00:16:10 +0700

    penambahan cover buku pada halaman admin

--- resources/views/admin/index.blade.php
+++ resources/views/admin/index.blade.php
@@ -36,7 +36,7 @@
                                             <div class="h5 mb-0 font-weight-bold text-gray-800">1.500 buku</div>
                                         </div>
                                         <div class="col-auto">
-                                            <i class="fas fa-calendar fa-2x text-gray-300"></i>
+                                            <i class="fas fa-book fa-2x text-gray-300"></i>
                                         </div>
                                     </div>
                                 </div>
@@ -54,7 +54,7 @@
                                             <div class="h5 mb-0 font-weight-bold text-gray-800">200 anggota</div>
                                         </div>
                                         <div class="col-auto">
-                                            <i class="fas fa-dollar-sign fa-2x text-gray-300"></i>
+                                            <i class="fas fa-folder-open fa-2x text-gray-300"></i>
                                         </div>
                                     </div>
                                 </div>
@@ -83,20 +83,20 @@
                                             </div>
                                         </div>
                                         <div class="col-auto">
-                                            <i class="fas fa-clipboard-list fa-2x text-gray-300"></i>
+                                            <i class="fas fa-user fa-2x text-gray-300"></i>
                                         </div>
                                     </div>
                                 </div>
                             </div>
                         </div>
                     </div>
-                         
-                        <div class="col-lg-5 mb-4 ">
-
-                            <!-- Illustrations -->
-                            <div class="card shadow mb-4">
+                        <!-- Konten Buku-->
+                         <div class="row">
+                            <!-- Buku1 -->
+                            <div class="col-lg-6 mb-4 ">
+                            <div class="card shadow mb-4 ">
                                 <div class="card-header py-3">
-                                    <h6 class="m-0 font-weight-bold text-primary">Karya Boy Chandra</h6>
+                                    <h6 class="m-0 font-weight-bold text-primary">Boy Chandra</h6>
                                 </div>
                                 <div class="card-body">
                                     <div class="text-center">
@@ -109,11 +109,11 @@
                                         Mulai dari soal memaknai arti kenangan, belajar untuk tidak mengulangi kesalahan yang sama lagi, mengatasi kesedihan akan luka atau kepahitan yang baru saja dialami, belajar untuk merelakan dan mengikhlaskan, 
                                         hingga memaknai arti cinta dengan perspektif yang lebih luas lagi.
                                          </a>
-                                        
+                                    </div>    
                                 </div>
                             </div>
-                            
-                            <!-- Illustrations -->
+                            <!-- Buku 2 -->
+                            <div class="col-lg-6 mb-4 ">
                             <div class="card shadow mb-4">
                                 <div class="card-header py-3">
                                     <h6 class="m-0 font-weight-bold text-primary">Tere Liye</h6>
@@ -124,15 +124,88 @@
                                             src="img/cover9.jpg" alt="...">
                                     </div>
                                     <p>Buku ini menceritakan tentang gadis berumur 12 tahun bernama Tania. 
-                                        Gadis kecil yang harus putus sekolah dan mengamen bersama adiknya sepanjang hari setelah 3 tahun lalu ayahnya pergi untuk selamanya. 
-                                        Tania, Dede (adiknya), dan ibunya tinggal di sebuah rumah kardus di pinggir kota akibat kesulitan ekonomi.
-
+                                      Gadis kecil yang harus putus sekolah dan mengamen bersama adiknya sepanjang hari setelah 3 tahun lalu ayahnya pergi untuk selamanya. 
+                                        Tania, Dede (adiknya), dan ibunya tinggal di sebuah rumah kardus di pinggir kota akibat kesulitan ekonomi.                     
+                                        
                                         </a>
                                         
                                 </div>
                             </div>
-                            
-                            
+                        </div>
+                             <!-- Buku 3 -->
+                            <div class="col-lg-3 mb-4 ">
+                            <div class="card shadow mb-4">
+                                <div class="card-header py-3">
+                                    <h6 class="m-0 font-weight-bold text-primary">J.R.R Tolkien</h6>
+                                </div>
+                                <div class="card-body">
+                                    <div class="text-center">
+                                        <img class="img-fluid px-3 px-sm-4 mt-3 mb-4" style="width: 17rem;"
+                                            src="img/thehobbit.jpg" alt="...">
+                                    </div>
+                                    <p>
+                
+                                        </a>
+                                        
+                                </div>
+                            </div>
+                        </div>
+                         <!-- Buku 4 -->
+                         <div class="col-lg-3 mb-4 ">
+                            <div class="card shadow mb-4">
+                                <div class="card-header py-3">
+                                    <h6 class="m-0 font-weight-bold text-primary">George Orwell</h6>
+                                </div>
+                                <div class="card-body">
+                                    <div class="text-center">
+                                        <img class="img-fluid px-3 px-sm-4 mt-3 mb-4" style="width: 25rem;"
+                                            src="img/animalfarm.jpg" alt="...">
+                                    </div>
+                                    <p>
+                
+                                        </a>
+                                        
+                                </div>
+                            </div>
+                        </div>
+                         <!-- Buku 5 -->
+                         <div class="col-lg-3 mb-4 ">
+                            <div class="card shadow mb-4">
+                                <div class="card-header py-3">
+                                    <h6 class="m-0 font-weight-bold text-primary">Neil Gaiman</h6>
+                                </div>
+                                <div class="card-body">
+                                    <div class="text-center">
+                                        <img class="img-fluid px-3 px-sm-4 mt-3 mb-4" style="width: 25rem;"
+                                            src="img/norse.jpg" alt="...">
+                                    </div>
+                                    <p>
+                
+                                        </a>
+                                        
+                                </div>
+                            </div>
+                        </div>
+                            <!-- Buku 6 -->
+                            <div class="col-lg-3 mb-4 ">
+                            <div class="card shadow mb-4">
+                                <div class="card-header py-3">
+                                    <h6 class="m-0 font-weight-bold text-primary">Charles Fernyhough</h6>
+                                </div>
+                                <div class="card-body">
+                                    <div class="text-center">
+                                        <img class="img-fluid px-3 px-sm-4 mt-3 mb-4" style="width: 25rem;"
+                                            src="img/piecesoflight.jpg" alt="...">
+                                    </div>
+                                    <p>
+                
+                                        </a>
+                                        
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+
                     
                         
 
