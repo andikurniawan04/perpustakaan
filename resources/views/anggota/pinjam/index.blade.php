9edf0ec6ca1ac99f04b4c1778880bcf78aee104b
Andi Kurniawan <65803684+andikurniawan04@users.noreply.github.com>
2022-03-01 17:38:28 +0700

    add pinjam buku anggota

---
+++ resources/views/anggota/pinjam/index.blade.php
@@ -0,0 +1,103 @@
+@extends('layouts/app')
+
+@section('content')
+    @if (session('Berhasil'))
+        <div class="alert alert-info alert-dismissible fade show" role="alert">
+            {{ session('Berhasil') }}
+            <button type="button" class="close" data-dismiss="alert" aria-label="Close">
+                <span aria-hidden="true">&times;</span>
+            </button>
+        </div>
+    @endif
+    @if (session('Gagal'))
+        <div class="alert alert-danger alert-dismissible fade show" role="alert">
+            {{ session('Gagal') }}
+            <button type="button" class="close" data-dismiss="alert" aria-label="Close">
+                <span aria-hidden="true">&times;</span>
+            </button>
+        </div>
+    @endif
+
+    <!-- Page Wrapper -->
+    <div id="wrapper">
+
+        <!-- Sidebar -->
+        @include('partials.sidebar')
+        <!-- End of Sidebar -->
+
+        <!-- Content Wrapper -->
+        <div id="content-wrapper" class="d-flex flex-column">
+
+            <!-- Main Content -->
+            <div id="content">
+
+                <!-- Topbar -->
+                @include('partials.topbar')
+                <!-- End of Topbar -->
+
+                <!-- Begin Page Content -->
+                <div class="container-fluid">
+
+                    <!-- Page Heading -->
+                    <div class="d-sm-flex align-items-center justify-content-between mb-4">
+                        <h1 class="h3 mb-0 text-gray-800">Buku</h1>
+                    </div>
+                    <div class="card shadow mb-4">
+                        <div class="card-header py-3">
+                            <h6 class="m-0 font-weight-bold text-primary">List Buku</h6>
+                        </div>
+                        <div class="card-body">
+                            <div class="table-responsive">
+                                <table class="table table-bordered" id="dataTable" width="100%" cellspacing="0">
+                                    <a href="{{ route('kategori.create') }}" class="btn btn-primary"><i
+                                            class="">New
+                                            Data</i></a>
+                                    <thead>
+                                        <tr>
+                                            <th>No</th>
+                                            <th>Kategori</th>
+                                            <th>Judul</th>
+                                            <th>Pengarang</th>
+                                            <th>Penerbit</th>
+                                            <th>Aksi</th>
+                                        </tr>
+                                    </thead>
+                                    <tbody>
+
+                                        @foreach ($buku as $row)
+                                            <tr>
+                                                <th scope="buku">{{ $loop->iteration }}</th>
+                                                <td>{{ $row->category->keterangan }}</td>
+                                                <td>{{ $row->judul_buku }}</td>
+                                                <td>{{ $row->pengarang }}</td>
+                                                <td>{{ $row->penerbit }}</td>
+                                                <td>
+                                                    <a class="btn btn-info"
+                                                        href="{{ route('kategori.edit', $row->id_buku) }}">Pinjam</a>
+                                                </td>
+                                            </tr>
+                                        @endforeach
+                                    </tbody>
+                                </table>
+                            </div>
+                        </div>
+                    </div>
+
+
+                </div>
+                <!-- End of Content Wrapper -->
+
+            </div>
+            <!-- End of Page Wrapper -->
+
+            <!-- Scroll to Top Button-->
+            <a class="scroll-to-top rounded" href="#page-top">
+                <i class="fas fa-angle-up"></i>
+            </a>
+            <!-- Footer -->
+            @include('partials.footer')
+            <!-- End of Footer -->
+
+            <!-- Logout Modal-->
+            @include('partials.logout')
+        @endsection
