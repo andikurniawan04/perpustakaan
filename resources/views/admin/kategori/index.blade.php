51e7d455b90683f1ce0448837a566aa9088e459c
Maulana <mauzen.rs@gmail.com>
2022-03-04 07:59:18 +0700

    Memperbaiki blade

--- resources/views/admin/kategori/index.blade.php
+++ resources/views/admin/kategori/index.blade.php
@@ -17,89 +17,55 @@
             </button>
         </div>
     @endif
+    <!-- Begin Page Content -->
+    <div class="container-fluid">
 
-    <!-- Page Wrapper -->
-    <div id="wrapper">
-
-        <!-- Sidebar -->
-        @include('partials.sidebar')
-        <!-- End of Sidebar -->
-
-        <!-- Content Wrapper -->
-        <div id="content-wrapper" class="d-flex flex-column">
-
-            <!-- Main Content -->
-            <div id="content">
-
-                <!-- Topbar -->
-                @include('partials.topbar')
-                <!-- End of Topbar -->
-
-                <!-- Begin Page Content -->
-                <div class="container-fluid">
-
-                    <!-- Page Heading -->
-                    <div class="d-sm-flex align-items-center justify-content-between mb-4">
-                        <h1 class="h3 mb-0 text-gray-800">Kategori</h1>
-                    </div>
-                    <div class="card shadow mb-4">
-                        <div class="card-header py-3">
-                            <h6 class="m-0 font-weight-bold text-primary">Data Kategori</h6>
-                        </div>
-                        <div class="card-body">
-                            <div class="table-responsive">
-                                <table class="table table-bordered" id="dataTable" width="100%" cellspacing="0">
-                                    <a href="{{ route('kategori.create') }}" class="btn btn-primary"><i
-                                            class="">New
-                                            Data</i></a>
-                                    <thead>
-                                        <tr>
-                                            <th>No</th>
-                                            <th>Nama Kategori</th>
-                                            <th>Aksi</th>
-                                        </tr>
-                                    </thead>
-                                    <tbody>
-
-                                        @foreach ($kategori as $row)
-                                            <tr>
-                                                <td>{{ $loop->iteration }}</td>
-                                                <td>{{ $row->keterangan }}</td>
-                                                <td class="d-flex flex-row justify-content-center">
-
-                                                    <form action="{{ route('kategori.destroy', $row->id_kategori) }}"
-                                                        method="POST">
-                                                        <a class="btn btn-success mr-2"
-                                                            href="{{ route('kategori.edit', $row->id_kategori) }}">Edit</a>
-                                                        @csrf
-                                                        @method('DELETE')
-                                                        <button class="btn btn-danger" type="submit">Delete</button>
-                                                    </form>
-
-                                                </td>
-                                            </tr>
-                                        @endforeach
-                                    </tbody>
-                                </table>
-                            </div>
-                        </div>
-                    </div>
-
-
-                </div>
-                <!-- End of Content Wrapper -->
-
+        <!-- Page Heading -->
+        <div class="d-sm-flex align-items-center justify-content-between mb-4">
+            <h1 class="h3 mb-0 text-gray-800">Kategori</h1>
+        </div>
+        <div class="card shadow mb-4">
+            <div class="card-header py-3">
+                <h6 class="m-0 font-weight-bold text-primary">Data Kategori</h6>
             </div>
-            <!-- End of Page Wrapper -->
+            <div class="card-body">
+                <div class="table-responsive">
+                    <table class="table table-bordered" id="dataTable" width="100%" cellspacing="0">
+                        <a href="{{ route('kategori.create') }}" class="btn btn-primary"><i class="">New
+                                Data</i></a>
+                        <thead>
+                            <tr>
+                                <th>No</th>
+                                <th>Nama Kategori</th>
+                                <th>Aksi</th>
+                            </tr>
+                        </thead>
+                        <tbody>
 
-            <!-- Scroll to Top Button-->
-            <a class="scroll-to-top rounded" href="#page-top">
-                <i class="fas fa-angle-up"></i>
-            </a>
-            <!-- Footer -->
-            @include('partials.footer')
-            <!-- End of Footer -->
+                            @foreach ($kategori as $row)
+                                <tr>
+                                    <td>{{ $loop->iteration }}</td>
+                                    <td>{{ $row->keterangan }}</td>
+                                    <td class="d-flex flex-row justify-content-center">
 
-            <!-- Logout Modal-->
-            @include('partials.logout')
-        @endsection
+                                        <form action="{{ route('kategori.destroy', $row->id_kategori) }}" method="POST">
+                                            <a class="btn btn-success mr-2"
+                                                href="{{ route('kategori.edit', $row->id_kategori) }}">Edit</a>
+                                            @csrf
+                                            @method('DELETE')
+                                            <button class="btn btn-danger" type="submit">Delete</button>
+                                        </form>
+
+                                    </td>
+                                </tr>
+                            @endforeach
+                        </tbody>
+                    </table>
+                </div>
+            </div>
+        </div>
+
+
+    </div>
+    <!-- End of Content Wrapper -->
+@endsection
